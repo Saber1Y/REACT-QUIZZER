176f6cd385509aae36f3a692172c60de122a2487
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-05 14:03:52 +0100

    UPDATE: CUSTOM STYLES ADDED

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#001e4d',
+        secondaryBtn: '#222',
+        secondaryHover: '#ff8c66',
+        timecol: '#ff5733',
+
+      },
+      fontFamily: {
+        pops: ['poppins', 'sans-serif'],
+        seoge: ["Segoe UI", 'Tahoma', 'Geneva', 'Verdana', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
