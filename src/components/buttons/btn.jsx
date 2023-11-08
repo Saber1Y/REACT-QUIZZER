b8ae03256e21db291d509a2f73bcce6051bcd88e
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-08 22:21:39 +0100

    UPDATE: DEV ERROR

--- src/components/buttons/btn.jsx
+++ src/components/buttons/btn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import rotate from '../../assests/rotate.png'
+// import rotate from '../../assests/rotate.png'
 
 const btn = () => {
   return (
--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -7,6 +7,8 @@ const Quiz = () => {
 <div className='bg-primary min-h-screen flex flex-col items-center justify-center'>
   <div className='bg-white w-full max-w-[600px] rounded-lg p-6'>
     <h1 className='text-center text-2xl font-semibold text-primary pb-8 font-pops'>Quizzer</h1>
+    <hr className='border-primary my-1' />
+    <h1>Hii</h1>
   </div>
   </div>
 </>
