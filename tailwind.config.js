aa4a0974305eaed4d5cdc0c676ae379f1b27e9fc
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-09 22:03:10 +0100

    UPDATE: FAILED DEV UPDATE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: '#001e4d',
-//         secondaryBtn: '#222',
-//         secondaryHover: '#ff8c66',
-//         timecol: '#ff5733',
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#001e4d',
+        secondaryBtn: '#222',
+        secondaryHover: '#ff8c66',
+        timecol: '#ff5733',
 
-//       },
-//       fontFamily: {
-//         pops: ['poppins', 'sans-serif'],
-//         seoge: ["Segoe UI", 'Tahoma', 'Geneva', 'Verdana', 'sans-serif']
-//       }
-//     },
-//   },
-//   plugins: [],
-// }
+      },
+      fontFamily: {
+        pops: ['poppins', 'sans-serif'],
+        seoge: ["Segoe UI", 'Tahoma', 'Geneva', 'Verdana', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
