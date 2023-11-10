165e406b090d36e35e51d1546ed260233228a01a
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-11 00:17:39 +0100

    CREATE': QUESTIONS DATA

--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -5,10 +5,10 @@ const Quiz = () => {
   return (
 <>
 <div className='bg-primary min-h-screen flex flex-col items-center justify-center'>
-  <div className='w-full max-w-[600px] rounded-lg p-6'>
+  <div className='w-full max-w-[600px] bg-white rounded-lg p-6'>
     <h1 className='text-center text-2xl font-semibold text-primary pb-8 font-pops'>Quizzer</h1>
     <hr className='border-primary my-1' />
-    <h1 className='text-timecol'>Hii</h1>
+      <h2 className='flex justify-center  items-center text-2xl font-semibold text-primary pb-4'>Question goes here: </h2>
   </div>
   </div>
 </>
---
+++ src/components/quizData.js
@@ -0,0 +1,98 @@
+const question = [
+    {
+        question: 'which type of memory allows us to unconsciously know how to tie our shoe lace?',
+        answers: [
+            { text: "Procedural Memory", correct: true },
+            { text: "Semantic Memory", correct: false },
+            { text: "Declarative Memory", correct: false },
+            { text: "Episody Memory", correct: false },
+        ]
+    },
+    {
+
+        question: 'which tree is known for it"s extremely thick trunk?',
+        answers: [
+            { text: "umbrella", correct: false },
+            { text: "tampoli", correct: false },
+            { text: "Baobab", correct: true },
+            { text: "mopane", correct: false },
+        ]
+    },
+    {
+
+
+        question: "Who is the Head of goverment in Germany?",
+        answers: [
+            { text: "Queen", correct: false },
+            { text: "Prime Minister", correct: false },
+            { text: "Chancellor", correct: true },
+            { text: "Premier", correct: false },
+        ]
+    },
+
+    {
+        question: 'A Biological Cell Must first create a replica of its DNA before it can?',
+        answers: [
+            { text: "Fight Infection", correct: false },
+            { text: "undergo Apoptosis", correct: false },
+            { text: "signal to other cells", correct: false },
+            { text: "divide", correct: true },
+        ]
+    },
+    {
+        question: 'What is the capital of France?',
+        answers: [
+            { text: 'Paris', correct: true },
+            { text: 'London', correct: false },
+            { text: 'Berlin', correct: false },
+            { text: 'Madrid', correct: false },
+        ]
+    },
+    {
+        question: 'Which planet is known as the "Red Planet"?',
+        answers: [
+            { text: 'Venus', correct: false },
+            { text: 'Mars', correct: true },
+            { text: 'Jupiter', correct: false },
+            { text: 'Saturn', correct: false },
+        ]
+    },
+    {
+        question: 'What is the chemical symbol for water?',
+        answers: [
+            { text: 'H2O', correct: true },
+            { text: 'CO2', correct: false },
+            { text: 'O2', correct: false },
+            { text: 'CH4', correct: false },
+        ]
+    },
+    {
+        question: 'Who wrote the play "Romeo and Juliet"?',
+        answers: [
+            { text: 'William Shakespeare', correct: true },
+            { text: 'Charles Dickens', correct: false },
+            { text: 'Jane Austen', correct: false },
+            { text: 'Mark Twain', correct: false },
+        ]
+    },
+    {
+        question: 'Which country is known as the "Land of the Rising Sun"?',
+        answers: [
+            { text: 'China', correct: false },
+            { text: 'Japan', correct: true },
+            { text: 'South Korea', correct: false },
+            { text: 'Thailand', correct: false },
+        ]
+    },
+    {
+        question: 'What is the largest mammal on Earth?',
+        answers: [
+            { text: 'Elephant', correct: false },
+            { text: 'Giraffe', correct: false },
+            { text: 'Blue Whale', correct: true },
+            { text: 'Hippopotamus', correct: false },
+        ]
+    }
+];
+
+export default { question };
