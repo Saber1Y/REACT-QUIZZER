0a74cd81582975e43b35e5ec9aff840f1e512b31
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-11 09:12:06 +0100

    UPDATE: DISPLAYED QUESTION

--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -11,26 +11,31 @@ const Quiz = () => {
   const optionData = Questions.Questions[option]
 
   return (
-<>
-<div className='bg-primary min-h-screen flex flex-col items-center justify-center'>
-  <div className='w-full max-w-[600px] bg-white rounded-lg p-6'>
-    <h1 className='text-center text-2xl font-semibold text-primary pb-8 font-pops'>Quizzer</h1>
-    <hr className='border-primary my-1' />
-    {Questions.Questions.map((question, index) => (
-          <div key={index} className='flex justify-center items-center text-primary pb-4'>
-            <span className='font-semibold text-2xl'>{index + 1}. </span>
-            <span className='text-2xl font-semibold'>{question.question}</span>
+    <>
+      <div className='bg-primary min-h-screen flex flex-col items-center justify-center'>
+        <div className='w-full max-w-[600px] bg-white rounded-lg p-6'>
+          <h1 className='text-center text-2xl font-semibold text-primary pb-8 font-pops'>Quizzer</h1>
+          <hr className='border-primary my-1' />
+          <div key={option} className='flex justify-center items-center text-primary pb-4'>
+            <span className='font-semibold text-2xl'>{option + 1}. </span>
+            <span className='text-2xl font-semibold'>{optionData.question}</span>
+          </div>
+        <div>
+         <div id='answers-button'>
+          {optionData.answers.map((answer, index) => {
+            return <Btn key={index} text={answer.text}/>
+          })}
+        </div>
+        <button
+          onClick={handleNext}
+          className='bg-primary text-white p-2 rounded-md mt-4 cursor-pointer hover:bg-blue-700 transition duration-300'
+        >
+          Next
+        </button>
          </div>
-          ))}
-  </div>
-  <div id='answers-button'>
-  {optionData.answers.map((answer, index) => {
-  return <Btn key={index} text={answer.text}/>
-})}
-
-  </div>
-</div>
-</>
+        </div>
+      </div>
+    </>
   )
 }
 
