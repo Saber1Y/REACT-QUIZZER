765bcffba19f6957de6143a09b4569d9761c3678
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-12 14:24:54 +0100

    UPDATE: btn update

--- src/components/buttons/btn.jsx
+++ src/components/buttons/btn.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const Btn = ({ text, onclick }) => {
+const Btn = ({ text, onClick, isCorrect }) => {
   return (
     <button
-      className='bg-white text-222 font-medium w-full border border-222 p-2 my-2 text-left rounded-md cursor-pointer transition duration-300'
-      onClick={onclick}
+      className={`hover:bg-black hover:text-white font-medium  w-full border p-2 my-2 text-left rounded-md cursor-pointer ${
+        isCorrect ? 'hover:bg-green' : 'hover:bg-red'
+      }`}
+      onClick={() => onClick(isCorrect)}
     >
       {text}
     </button>
   );
 };
+
 
 export default Btn;
--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -4,6 +4,7 @@ import Btn from '../buttons/Btn'
 
 const Quiz = () => {
   const [option, setOption] = useState(0)
+  const [score, setScore] = useState(0);
 
   const handleNext = () => {
     setOption(option + 1)
@@ -14,25 +15,21 @@ const Quiz = () => {
     <>
       <div className='bg-primary min-h-screen flex flex-col items-center justify-center'>
         <div className='w-full max-w-[600px] bg-white rounded-lg p-6'>
-          <h1 className='text-center text-2xl font-semibold text-primary pb-8 font-pops'>Quizzer</h1>
+          <h1 className='text-center text-2xl font-semibold text-primary pb-5 font-pops'>Quizzer</h1>
           <hr className='border-primary my-1' />
           <div key={option} className='flex justify-center items-center text-primary pb-4'>
-            <span className='font-semibold text-2xl'>{option + 1}. </span>
+            <span className='font-semibold text-2xl mr-3'>{option + 1}. </span>
             <span className='text-2xl font-semibold'>{optionData.question}</span>
           </div>
-        <div>
+            <div>
          <div id='answers-button'>
-          {optionData.answers.map((answer, index) => {
-            return <Btn key={index} text={answer.text}/>
-          })}
+            {optionData.answers.map((answer, index) => {
+              return <Btn key={index} text={answer.text} onClick={handleNext}/>
+            })}
         </div>
-        <button
-          onClick={handleNext}
-          className='bg-primary text-white p-2 rounded-md mt-4 cursor-pointer hover:bg-blue-700 transition duration-300'
-        >
-          Next
-        </button>
-         </div>
+       <Btn text="Next" />
+       
+            </div>
         </div>
       </div>
     </>
