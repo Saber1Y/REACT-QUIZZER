94625a5fca7e794482ed5aa63f8677bedf8ea8d1
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-25 19:14:57 +0100

    CREATE: RULES DATA

--- src/components/Rulee/Rules.jsx
+++ src/components/Rulee/Rules.jsx
@@ -7,39 +7,39 @@ const Rules = () => {
       <h2 className='text-semibold text-white text-base'>Rules</h2>
       <hr className='1px solid bg-orange-600 pt-6'/>
       <div>
-        <ol>
-          <li className='text-xl text-white my-2 font-seoge'>
+        <ol className='px-20 my-4'>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Quiz Duration: Each quiz session has a time limit of 5 minutes (300
             seconds). Players must answer as many questions as they can within
             the time limit.
           </li>
-          <li>Scoring: Players earn one point for each correct answer.</li>
-          <li>
+          <li className='text-2xl text-white my-2 font-seoge'>Scoring: Players earn one point for each correct answer.</li>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Answer Selection: Once a player selects an answer, they cannot
             change it. So, think carefully before making a choice.
           </li>
-          <li>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Time Limit per Question: Players have a maximum of 30 seconds to
             answer each question. If the time runs out, the question will be
             considered unanswered, and the next question will be displayed.
           </li>
-          <li>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Passing: There is no option to skip or pass a question. You must
             answer each question to proceed to the next one.
           </li>
-          <li>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Feedback: After selecting an answer, you will immediately receive
             feedback on whether your answer was correct or not.
           </li>
-          <li>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Game Over: The quiz ends automatically when the time runs out or
             after answering all the questions.
           </li>
-          <li>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Try Again: If you wish to play again, click the "Try Again" button
             at the end of the quiz to reset the game and start a new session.
           </li>
-          <li>
+          <li className='text-2xl text-white my-2 font-seoge'>
             Have Fun: Most importantly, have fun while testing your knowledge
             and challenging yourself to improve your score!
           </li>
---
+++ src/components/rulesData.js
@@ -0,0 +1,40 @@
+const rulesData = [
+    {
+        id: 1,
+        text: "Quiz Duration: Each quiz session has a time limit of 5 minutes (300 seconds). Players must answer as many questions as they can within the time limit.",
+    },
+    {
+        id: 2,
+        text: "Scoring: Players earn one point for each correct answer.",
+    },
+    {
+        id: 3,
+        text: "Answer Selection: Once a player selects an answer, they cannot change it. So, think carefully before making a choice.",
+    },
+    {
+        id: 4,
+        text: "Time Limit per Question: Players have a maximum of 30 seconds to answer each question. If the time runs out, the question will be considered unanswered, and the next question will be displayed.",
+    },
+    {
+        id: 5,
+        text: "Passing: There is no option to skip or pass a question. You must answer each question to proceed to the next one.",
+    },
+    {
+        id: 6,
+        text: "Feedback: After selecting an answer, you will immediately receive feedback on whether your answer was correct or not.",
+    },
+    {
+        id: 7,
+        text: "Game Over: The quiz ends automatically when the time runs out or after answering all the questions.",
+    },
+    {
+        id: 8,
+        text: 'Try Again: If you wish to play again, click the "Try Again" button at the end of the quiz to reset the game and start a new session.',
+    },
+    {
+        id: 9,
+        text: "Have Fun: Most importantly, have fun while testing your knowledge and challenging yourself to improve your score!",
+    },
+];
+
+export default rulesData;
