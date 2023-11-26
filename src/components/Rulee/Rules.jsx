d224c3a47f8c2f9478232e59d395951fa3b0d816
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-26 20:05:52 +0100

    UPDATE: RULES STYLED COMP

--- src/components/Rulee/Rules.jsx
+++ src/components/Rulee/Rules.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import rulesData from '../rulesData'
-
+import React from 'react';
+import rulesData from '../rulesData';
 
 const Rules = () => {
   return (
     <>
-    <h2 className='text-semibold text-white text-center text-3xl py-5'>Rules</h2>
-    <hr className=' bg-orange-600 mx-16'/>
-        <div className='items-center text-center max-w-md p-5 rounded-lg box-border '>
-          <ol>
-
-          </ol>
-        </div>
+      <h2 className='text-bold text-orange-600 text-center text-5xl py-6'>Rules</h2>
+      <hr className='bg-orange-600 mx-16' />
+      <div className='flex items-center text-center mx-12  p-8 mt-5  border border-solid border-white rounded-lg shadow-2xl'>
+        <ol className='text-white font-seoge text-xl mx-auto my-auto h-auto'>
+          {rulesData.map((rule) => (
+            <li key={rule.id} className=''>{rule.text}</li>
+          ))}
+        </ol>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Rules
+export default Rules;
