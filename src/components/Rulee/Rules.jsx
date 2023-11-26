1e7fcb6df86b0d592eae1dc1cea3cb45e0385eeb
ayodeji <ayodejiakintobi1@gmail.com>
2023-11-26 20:55:50 +0100

    UPDATE: WELCOME TXT STYLED

--- src/components/Rulee/Rules.jsx
+++ src/components/Rulee/Rules.jsx
@@ -7,9 +7,9 @@ const Rules = () => {
       <h2 className='text-bold text-orange-600 text-center text-5xl py-6'>Rules</h2>
       <hr className='bg-orange-600 mx-16' />
       <div className='flex items-center text-center mx-12  p-8 mt-5  border border-solid border-white rounded-lg shadow-2xl'>
-        <ol className='text-white font-seoge text-xl mx-auto my-auto h-auto'>
+        <ol className='text-white  font-seoge text-xl mx-auto my-auto h-auto'>
           {rulesData.map((rule) => (
-            <li key={rule.id} className=''>{rule.text}</li>
+            <li key={rule.id} className='pr-0'>{rule.text}</li>
           ))}
         </ol>
       </div>
--- src/index/Welcome.jsx
+++ src/index/Welcome.jsx
@@ -1,38 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
-    <div className='welcome'>
-      <h1 className='text-2xl font-bold mb-4'>Welcome to Quizzer!</h1>
-        <h3 className='text-xl mb-4'>Welcome to Quizzer - Test Your Knowledge!</h3>
-        <p>
-            Hello there, quiz enthusiasts! Are you ready to embark on an exciting
-            journey of knowledge and fun? Welcome to Quizzer, the ultimate quiz game
-            designed to challenge your intellect and test your expertise on various
-            topics. Get ready to explore fascinating questions and unlock the depths
-            of your understanding. Whether you're a seasoned quizzer or a newcomer
-            eager to learn, this quiz will entertain and educate you. Challenge
-            yourself to answer each question correctly, and watch your score soar to
-            new heights. But remember, time is of the essence, so think quickly and
-            trust your instincts. Are you up for the challenge? Gather your wits,
-            flex those brain muscles, and let's dive into the world of Quizzer!
-            Happy quizzing!
-        </p>
+    <div className='text-center max-w-2xl mx-auto box-border border-white border shadow-2xl rounded-xl border-solid'>
+      <h1 className='text-3xl text-white font-bold mb-2'>Welcome to Quizzer!</h1>
+      <h3 className='text-xl mb-2 text-orange-500'>Welcome to Quizzer - Test Your Knowledge!</h3>
+      <p className='text-white text-2xl font-bold px-14'>
+        Hello there, quiz enthusiasts! Are you ready to embark on an exciting journey of knowledge and fun?
+        Welcome to Quizzer, the ultimate quiz game designed to challenge your intellect and test your expertise on various topics.
+        Get ready to explore fascinating questions and unlock the depths of your understanding. Whether you're a seasoned quizzer or a newcomer
+        eager to learn, this quiz will entertain and educate you. Challenge yourself to answer each question correctly, and watch your score soar to
+        new heights. But remember, time is of the essence, so think quickly and trust your instincts. Are you up for the challenge? Gather your wits,
+        flex those brain muscles, and let's dive into the world of Quizzer! Happy quizzing!
+      </p>
+      <div className='my-2'>
       <Link
         to="/quiz"
-        className="bg-green-500 text-white py-2 px-4 rounded-full inline-block hover:bg-green-600"
-          >
+        className="bg-orange-600 text-white py-2 px-4 rounded-full inline-block hover:bg-orange6600 mt-4"
+      >
         Start Quiz
-        </Link>
+      </Link>
       <Link
         to="/Rules"
-        className="bg-blue-500 text-white py-2 px-4 rounded-full inline-block hover:bg-blue-600"
-        >
+        className="bg-orange-600 text-white py-2 px-4 rounded-full inline-block hover:bg-blue-600 mt-4 ml-4"
+      >
         Rules
-        </Link>
+      </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
